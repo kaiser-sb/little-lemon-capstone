cf000ffb472c8367222a6915c5bfe1fc1720610e
Sunetra Bhowmick <matrix_hunt@ymail.com>
2024-03-08 11:40:35 +0530

    Lazy loading enabled

--- src/components/ConfirmedBooking.js
+++ src/components/ConfirmedBooking.js
@@ -9,7 +9,7 @@ function ConfirmedBooking () {
             <section className="wrapper confirmed-section">
                 <img src={require("../assets/images/confirmed_label.png")} alt="confirmation logo" loading="lazy" />
                 <h1>Your booking has been confirmed.</h1>
-                <p>Thank you for celebrating with us!</p>
+                <p>Thank you for celebrating with us!</p> 
                 <Link to="/"><button className="button">Return to Home</button></Link>
             </section>
     )
